19e001eb5a9a4d51aa944804c74b02ec8209b4db
Prabhjeet Singh <prabhjeet.singh92@outlook.com>
2026-07-31 08:50:20 +0530

    added tracing

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ class Settings(BaseSettings):
     openrouter_api_key: str
     openrouter_model:str
     ollama_model:str
+    langfuse_public_key:str
+    langfuse_base_url:str
+    langfuse_secret_key:str
 
     model_config = SettingsConfigDict(
         env_file=".env",
--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from langchain_core.prompts import PromptTemplate
 from llm import get_llm,get_ollama_llm
+from langfuse.langchain import CallbackHandler
+from langfuse import get_client
+import os
+from config import settings
 
 def main():
+
+    os.environ.setdefault("LANGFUSE_PUBLIC_KEY", settings.langfuse_public_key)
+    os.environ.setdefault("LANGFUSE_SECRET_KEY", settings.langfuse_secret_key)
+    os.environ.setdefault("LANGFUSE_HOST", settings.langfuse_base_url)
+
+    langfuse_handler = CallbackHandler()
 
     #llm = get_llm()
     
     llm =get_ollama_llm()
-    
+
     prompt_message="Tell me a startup idea that can help me half a million dollars quickly and tell me which llm(exact name) am i talking to, and is it free"
     
     input_prompt_template=PromptTemplate(
@@ -15,10 +25,11 @@ def main():
         )
 
     chain=input_prompt_template | llm # langchain expression language first part(promptTemplate to be fed to second part(llm))
-    response = chain.invoke({})
+    response = chain.invoke({}, config={"callbacks": [langfuse_handler]}  )
 
     print(response.content)
 
-
+    langfuse = get_client()
+    langfuse.flush()
 
 main()
