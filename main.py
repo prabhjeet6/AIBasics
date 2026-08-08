f2f94c42ef033074ae166afc90311aee60f24373
Prabhjeet Singh <prabhjeet.singh92@outlook.com>
2026-08-08 19:49:33 +0530

    updated files

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
-from langchain_core.prompts import PromptTemplate
-from llm import get_llm,get_ollama_llm
+#from langchain_core.prompts import PromptTemplate
+from langchain_core.messages import HumanMessage
+
+from agentutils import get_llm,get_ollama_llm, get_agent
 from tracer import get_tracer,flush_tracer
 
-from langchain.agents import create_agent
-from langchain.tools import tool
-from langchain_core.messages import HumanMessage
-from langchain_tavily import TavilySearch
 
-import os
-from config import settings
 
-os.environ.setdefault("TAVILY_API_KEY", settings.tavily_api_key)
 
 def main():
 
     tracing_handler = get_tracer()
-
-    llm = get_llm()
     
-    #llm =get_ollama_llm() # ollama does not support Tools
-
-    tools=[TavilySearch()]
-
-    agent=create_agent(model=llm,tools=tools)
+    agent=get_agent()
 
     #prompt_message="Tell me a startup idea that can help me half a million dollars quickly and tell me which llm(exact name) am i talking to, and is it free"
     #input_prompt_template=PromptTemplate(
@@ -39,6 +28,7 @@ def main():
     config={
         "callbacks": [tracing_handler]
     })
+    
     print(response)
 
     flush_tracer()
